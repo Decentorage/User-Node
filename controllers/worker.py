3b627f3fdf6acdcb206e65965144de2a2dbba533
3amrA7med <amr.ahmed.abdelbaqi@gmail.com>
2021-07-23 03:59:00 +0200

    fix errors in resume download

--- controllers/worker.py
+++ controllers/worker.py
@@ -36,23 +36,23 @@ class Worker(QRunnable):
 
     @pyqtSlot()
     def run(self):
-        try:
-            if self.loading_screen_selected():
-                self.change_page_signal_emitter.change_waiting_text(self.ui.loading_text, self.text)
-                self.change_page_signal_emitter.change_page(self.ui.stackedWidget, self.ui.loading_page)
+        # try:
+        if self.loading_screen_selected():
+            self.change_page_signal_emitter.change_waiting_text(self.ui.loading_text, self.text)
+            self.change_page_signal_emitter.change_page(self.ui.stackedWidget, self.ui.loading_page)
 
-            output = self.fn(*self.args, **self.kwargs)
+        output = self.fn(*self.args, **self.kwargs)
 
-            if self.loading_screen_selected():
-                if output == 'failure':
-                    self.change_page_signal_emitter.change_page(self.ui.stackedWidget, self.current_page)
-                else:
-                    self.change_page_signal_emitter.change_page(self.ui.stackedWidget, self.target_page)
-
-        except Exception as e:
-            worker_error_page("Error", str(e), self.ui)
-            if self.loading_screen_selected():
+        if self.loading_screen_selected():
+            if output == 'failure':
                 self.change_page_signal_emitter.change_page(self.ui.stackedWidget, self.current_page)
+            else:
+                self.change_page_signal_emitter.change_page(self.ui.stackedWidget, self.target_page)
+
+#         except Exception as e:
+#             worker_error_page("Error", str(e), self.ui)
+#             if self.loading_screen_selected():
+#                 self.change_page_signal_emitter.change_page(self.ui.stackedWidget, self.current_page)
 
 
 def worker_error_page(title, body, ui, target=None):
